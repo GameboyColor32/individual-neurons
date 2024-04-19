21f35d27f152be7c6e68ff762ffddf792717a84e
Vindevogel Victor <victor.vindevogel@epitech.eu>
2024-04-19 23:11:38 +0200

    [+] individual neurons visualization

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './individual_neurons.js',
+  './individual_neurons_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,41 @@
+use egui_plot::{Legend, Line, Plot, PlotPoints};
+use egui::Color32;
+
+const E_E: f64 = 55.0;
+const E_I: f64 = -70.0;
+const E_L: f64 = -70.0;
+
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    #[serde(skip)] // This how you opt-out of serialization of a field
-    value: f32,
+    #[serde(skip)]
+    excitation: f64,
+    #[serde(skip)]
+    delta_time: f64,
+    #[serde(skip)]
+    vm: f64,
+    #[serde(skip)]
+    tick: i32,
+    #[serde(skip)]
+    inhibition: f64,
+    #[serde(skip)]
+    history: Vec<[f64; 2]>,
+    #[serde(skip)]
+    net_history: Vec<[f64; 2]>,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+	    excitation: 0.0,
+	    delta_time: 0.0,
+	    vm: -70.0, // resting potential
+            tick: 0,
+            inhibition: 0.0,
+            history: vec![],
+            net_history: vec![],
         }
     }
 }
@@ -65,45 +86,85 @@ impl eframe::App for TemplateApp {
             });
         });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+	egui::SidePanel::left("left").resizable(true).show(ctx, |ui| {
+            ui.label("Constants");
+	    ui.label(format!("Excitatory channel E_e: {}", E_E));
+	    ui.label(format!("Inhibitory channel E_i: {}", E_I));
+	    ui.label(format!("Leak channel E_l: {}", E_L));
+	    ui.separator();
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
-
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
-
+            ui.label(format!("g_e: {}", self.excitation));
+            ui.label(format!("Delta time: {}", self.delta_time));
             ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+	    ui.add(egui::DragValue::new(&mut self.excitation).prefix("Excitation: ").clamp_range(0.0..=1.0).speed(0.1));
+	    ui.add(egui::DragValue::new(&mut self.delta_time).prefix("Dt: ").clamp_range(0.0..=1.0).speed(0.1));
+	    ui.separator();
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            if ui.button("Reset").clicked() {
+                self.history.clear();
+                self.net_history.clear();
+                self.vm = -70.0;
+                self.excitation = 0.0;
+                self.delta_time = 0.0;
+                self.tick = 0;
+                self.inhibition = 0.0;
+
+            }
+            if ui.button("Next tick").clicked() {
+                let i_net = compute_net_current(self.excitation, self.inhibition, self.vm);
+                let vm = compute_vm(self.vm, self.delta_time, i_net);
+                self.vm = vm;
+                self.history.push([self.tick as f64, vm]);
+                self.net_history.push([self.tick as f64, i_net]);
+                self.tick += 1;
+            }
+            ui.separator();
+
+            let s = if self.vm > E_I { format!("Vm: {}", self.vm) } else { format!("Vm at resting potential: {}", self.vm) };
+	    ui.label(s);
+	    
+	});
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+            ui.heading("graph");
+
+	    plot(ui, &self.history, &self.net_history);
         });
     }
 }
 
-fn powered_by_egui_and_eframe(ui: &mut egui::Ui) {
-    ui.horizontal(|ui| {
-        ui.spacing_mut().item_spacing.x = 0.0;
-        ui.label("Powered by ");
-        ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-        ui.label(" and ");
-        ui.hyperlink_to(
-            "eframe",
-            "https://github.com/emilk/egui/tree/master/crates/eframe",
-        );
-        ui.label(".");
+const G_L: f64 = 1.0;
+const G_BAR_E: f64 = 0.5;
+const G_BAR_I: f64 = 0.5;
+const G_BAR_L: f64 = 1.0;
+const LEAK: f64 = G_L * G_BAR_L;
+
+fn compute_net_current(g_e: f64, g_i: f64, vm: f64) -> f64 {
+    g_e * G_BAR_E * (E_E - vm) +
+        g_i * G_BAR_I * (E_I - vm) +
+        LEAK * (E_L - vm)
+}
+
+fn compute_vm(prev_vm: f64, dt: f64, i_net: f64) -> f64 {
+    prev_vm + dt * i_net
+}
+
+fn plot(ui: &mut egui::Ui, vm: &Vec<[f64; 2]>, net_hist: &Vec<[f64; 2]>) -> egui::Response {
+        let graph: Vec<[f64; 2]> = vm
+        .iter()
+        .map(|&[x, y]| [x, y])
+        .collect();
+
+    let net_graph: Vec<[f64; 2]> = net_hist
+        .iter()
+        .map(|&[x, y]| [x, y])
+        .collect();
+    let my_plot = Plot::new("My Plot").legend(Legend::default());
+    let inner = my_plot.show(ui, |plot_ui| {
+        plot_ui.line(Line::new(PlotPoints::from(graph)).name("V_m"));
+        plot_ui.line(Line::new(PlotPoints::from(net_graph)).color(Color32::LIGHT_BLUE).name("I_net"));
     });
+    inner.response
 }
